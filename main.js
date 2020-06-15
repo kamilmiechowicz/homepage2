fe3a99eb1ddc4fde2178a9f992f913ab4c8274b0
Kamil Miechowicz <kmiechowicz123@gmail.com>
2020-06-16 00:15:40 +0200

    initial

---
+++ main.js
@@ -0,0 +1,12 @@
+const button = document.querySelector('hamburgerMenu--js');
+console.log(button)
+const myClick =() =>{
+    console.log('kliknąłeś')
+}
+button.addEventListener('click',()=>{
+    const heading = document.querySelector("hamburgerMenu--js");
+    heading.innerHTML = 'Witamy w naszym sklepie';
+    heading.classList.add('')
+}
+
+heading.innerHTML
